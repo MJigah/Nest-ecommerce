d0373d3c4d3c077e57b1a9d438968e9cc670755b
MJigah <jigah4july@gmail.com>
2022-11-07 12:23:52 +0100

    updating login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -637,17 +637,10 @@ function Login() {
             <div className="row">
               <div className="col-xl-8 col-lg-10 col-md-12 m-auto">
                 <div className="row">
-                  <div className="col-lg-6 pr-30 d-none d-lg-block">
-                    <img
-                      className="border-radius-15"
-                      src="assets/imgs/page/login-1.png"
-                      alt=""
-                    />
-                  </div>
-                  <div className="col-lg-6 col-md-8">
+                  <div className="col-lg-12 col-md-8">
                     <div className="login_wrap widget-taber-content background-white">
                       <div className="padding_eight_all bg-white">
-                        <div className="heading_s1">
+                        <div className="heading_s1" style={{textAlign: "center"}}>
                           <h1 className="mb-5">Login</h1>
                           <p className="mb-30">
                             Don't have an account?{" "}
@@ -671,7 +664,7 @@ function Login() {
                               placeholder="Your password *"
                             />
                           </div>
-                          <div className="login_footer form-group">
+                          <div className="login_footer form-group" style={{textAlign: "center"}}>
                             <div className="chek-form">
                               <input
                                 type="text"
@@ -687,7 +680,7 @@ function Login() {
                               <b className="text-best">5</b>
                             </span>
                           </div>
-                          <div className="login_footer form-group mb-50">
+                          <div className="login_footer form-group mb-50" style={{textAlign: "center", alignItems: "center", justifyContent: "space-between"}}>
                             <div className="chek-form">
                               <div className="custome-checkbox">
                                 <input
@@ -714,6 +707,7 @@ function Login() {
                               type="submit"
                               className="btn btn-heading btn-block hover-up"
                               name="login"
+                              style={{width: "100%"}}
                             >
                               Log in
                             </button>
