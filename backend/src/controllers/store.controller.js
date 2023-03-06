07b239ef731977c3a18518f6d23f9d073cf3a7e4
Jigah Miracle <jigah4july@gmail.com>
2023-03-06 22:15:13 +0100

    Set Store Controllers

--- backend/src/controllers/store.controller.js
+++ backend/src/controllers/store.controller.js
@@ -6,12 +6,25 @@ const { Store, storeConverter } = require("../models/store.models");
 
 const createNewStore = asyncHandler(async (req, res) => {
   try {
-    const { name, address, isVerified, email, contact, category } = req.body;
+    const { name, address, isVerified, email, contact} = req.body;
 
     //Check for Existing Store
     const checkStore = await checkForStore(email);
     if(checkStore){
         return res.status(400).send({message: 'Store already exists!'})
+    }
+
+    //Create Store Categories
+    const category = {
+        name: "Food Bank",
+        menu: {
+            name: "Spice & Seasoning",
+            sub: [
+                {name: "salt"},
+                {name: "cubes"},
+                {name: "stocks"},
+            ]
+        }
     }
 
     //Query to create new firebase record
