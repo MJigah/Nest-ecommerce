7677c213afbcec4813886ac67bc372d7c9f77107
Jigah Miracle <jigah4july@gmail.com>
2023-03-09 23:58:06 +0100

    Models Update!

--- backend/src/helpers/store.helpers.js
+++ backend/src/helpers/store.helpers.js
@@ -17,3 +17,65 @@ const checkForStore = (async(email) => {
 module.exports = {
     checkForStore,
 }
+
+
+// User db
+// ==> Address
+
+// Category db
+// ==> Main Category
+//     ==> Sub Category
+//         ==> Sub sub Category
+//             ==> tags
+
+// Store db
+// ==> Category
+// ==> star Ratings
+// ==> Reviews
+
+// Product db
+// ==> Description
+// ==> star Ratings
+// ==> Reviews
+
+// Order db
+// ==> orderItems
+// ==> shippingAddress
+// ==> paymentMethod
+// ==> paymentResult
+// ==> shippingPrice
+// ==> taxPrice
+// ==> user
+// ==> isPaid
+// ==> paidAt
+// ==> isDelivered
+// ==> deliveredAt
+// orderItems: [
+//     {
+//       slug: { type: String, required: true },
+//       name: { type: String, required: true },
+//       quantity: { type: Number, required: true },
+//       image: { type: String, required: true },
+//       price: { type: Number, required: true },
+//       product: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Product',
+//         required: true,
+//       },
+//     },
+//   ],
+//   shippingAddress: {
+//     fullName: { type: String, required: true },
+//     address: { type: String, required: true },
+//     city: { type: String, required: true },
+//     postalCode: { type: String, required: true },
+//     country: { type: String, required: true },
+//     location: {
+//       lat: Number,
+//       lng: Number,
+//       address: String,
+//       name: String,
+//       vicinity: String,
+//       googleAddressId: String,
+//     },
+//   }
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,8 +1,9 @@
 class User{
-    constructor(email, username, password, createdAt, updatedAt){
+    constructor(email, username, password, createdAt, updatedAt, address){
         this.email = email;
         this.username = username;
         this.password = password;
+        this.address = address;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
@@ -14,13 +15,14 @@ const userConverter = {
             email: user.email,
             username: user.username,
             password: user.password,
+            address: user.address,
             createdAt: user.createdAt,
             updatedAt: user.updatedAt
             };
     },
     fromFirestore: (snapshot, options) => {
         const data = snapshot.data(options);
-        return new User(data.email, data.username, data.password, data.createdAt, data.updatedAt);
+        return new User(data.email, data.username, data.password, data.address, data.createdAt, data.updatedAt);
     }
 };
 
